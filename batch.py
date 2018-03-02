1e8e4338a292f949007e4ebd96a2c66ea89389f9
max0y <askmaxwork@gmail.com>
2018-03-02 16:44:49 +0800

    program structure modified, support for .json danmaku files

--- batch.py
+++ batch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# batch conversion of bilibili .xml files to .ass files
+# batch conversion of danmaku files to .ass files
 
 import os
 import sys
@@ -14,60 +14,81 @@ def safe_chdir(directory):
     except NotADirectoryError:
         print('\nNotADirectoryError: please input the right directory!')
     except:
-        print('\nUnexpected error:', sys.exc_info()[0])
-        raise
+        print('\nUnexpected error:', sys.exc_info()[0], 'please input the right directory!')
+        return False
     else:
         return True
 
-# directory = r'D:\download\video\xml'
-# size = '1920x1080'  # Stage size in pixels
-form = 'autodetect' # Format of input file
-fn = 'MS PGothic'   # Specify font face
-fs = '30'           # Default font size
-alpha = '1.0'       # Text opacity
-dm = '8'            # Duration of scrolling comment display
-ds = '5'            # Duration of still comment display
-fit = ''            # Regular expression to filter comments
-protect = '0'       # Reserve blank on the bottom of the stage
-reduce = False      # Reduce the amount of comments if stage is full
+
+def cmdlist(script_dir, danmaku_dir, size, form = 'autodetect', fn = 'sans-serif', fs = '25', alpha = '1.0', dm = '5', ds = '5', fit = '', protect = '0', reduce = False, ex_extension = 0):
+    # script_dir: dircectory of danmaku2ass.py
+    # danmaku_dir: dircectory of the original danmaku files
+    # form: format of input file (autodetect|Bilibili|Tudou2|MioMio|Acfun|Niconico|Tudou) [default: autodetect]
+    # size: stage size in pixels, eg: 1920x1080
+    # fn: specify font face [default: sans-serif]
+    # fs: font size [default: 25]
+    # alpha: Text opacity [default: 1.0]
+    # dm: duration of scrolling comment display [default: 5]
+    # ds: duration of still comment display [default: 5]
+    # fit: regular expression to filter comments
+    # protect: reserve blank on the bottom of the stage [default: 0]
+    # reduce: reduce the amount of comments if stage is full [default: False]
+
+    # ex_extension: extra extension in the danmaku files [default: 0]
+    # (eg: when converting *.cmt.xml to *.ass, the extra extension is .cmt, so ex_extension = 4)
+
+    if os.name == 'nt':
+        py_name = '\"' + sys.executable + '\" '
+        script_dir = script_dir + '\\'
+        danmaku_dir = danmaku_dir + '\\'
+    else:
+        py_name = 'python3 '
+        script_dir = script_dir + '/'
+        danmaku_dir = danmaku_dir + '/'
+    
+    files =  [x for x in os.listdir(danmaku_dir) if os.path.isfile(danmaku_dir + x) and os.path.splitext(danmaku_dir + x)[1] in ['.xml', '.json']]
+    command_list = []
+    for file in files:
+        file_extension = len(file.split('.')[-1]) + 1 + ex_extension           # file extension length
+        command = py_name + r'"' + script_dir + 'danmaku2ass.py' + r'"' \
+                + ' -f ' + form \
+                + ' -o ' + r'"' + file[:-file_extension] + '.ass' + r'"' \
+                + ' -s ' + size \
+                + ' -fn ' + r'"' + fn + r'"' \
+                + ' -fs ' + fs \
+                + ' -a ' + alpha \
+                + ' -dm ' + dm \
+                + ' -ds ' + ds \
+                + ' -p ' + protect
+        if fit:
+            command = command + ' -fl ' + '\"' + fit + '\"'
+        if reduce:
+            command = command + ' -r'
+        command = command + ' \"' + file + '\"'
+        command_list.append(command)
+
+    return command_list
 
 
-if os.name == 'nt':
-    py_name = '\"' + sys.executable + '\" '
-    current_dir = os.path.abspath(sys.argv[0][:-8]) + '\\'
-else:
-    py_name = 'python3 '
-    current_dir = os.path.abspath(sys.argv[0][:-8]) + '/'
+def main():
+    script_dir = os.path.abspath(sys.argv[0][:-8])
 
-directory = input('\nPlease input the directory of .xml files: ')
-while not safe_chdir(directory):
-    directory = input('\nPlease input the directory of .xml files: ')
+    danmaku_dir = input('\nPlease input the directory of your danmaku files: ')
+    while not safe_chdir(danmaku_dir):
+        danmaku_dir = input('\nPlease input the directory of your danmaku files: ')
 
-size = input(r'Please input the resolution of videos(eg:1920x1080): ')
-size = size.lower()
+    size = input('\nPlease input your stage size in pixels(eg:1920x1080): ')
+    size = size.lower()
+    
+    # you-get downloaded files, *.cmt.xml | *.cmt.json --> *.ass
+    # if you want to convert *.xml and *.json to *.ass, set  ex_extension to 0
+    command_list = cmdlist(script_dir, danmaku_dir, size, fn ='MS PGothic', dm = '8', ex_extension = 4)
 
-# list of .xml files in the directory
-files =  [x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1]=='.xml']
+    for cmd in command_list:
+        print('converting ', cmd.split('\"')[-2])
+        print(os.popen(cmd).read())
+    
+    input('press Enter to exit...')
 
-for file in files:
-    # you-get download files, *.cmt.xml --> *.ass
-    command = py_name + r'"' + current_dir + 'danmaku2ass.py' + r'"' \
-              + ' -f ' + form \
-              + ' -o ' + r'"' + file[:-8] + '.ass' + r'"' \
-              + ' -s ' + size \
-              + ' -fn ' + r'"' + fn + r'"' \
-              + ' -fs ' + fs \
-              + ' -a ' + alpha \
-              + ' -dm ' + dm \
-              + ' -ds ' + ds \
-              + ' -p ' + protect
-    if fit:
-        command = command + ' -fl ' + fit
-    if reduce:
-        command = command + ' -r'
 
-    command = command + r' "' + file + r'"'
-    print('converting ', file)
-    print(os.popen(command).read())
-
-input('press Enter to exit...')
+main()
